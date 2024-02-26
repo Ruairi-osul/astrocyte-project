80db9950fe0fd0c2990fe37953f3224bf69b777e
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2024-02-26 15:21:43 -0500

    Added documentation to group transforms

--- astro/transforms/groups.py
+++ astro/transforms/groups.py
@@ -8,6 +8,17 @@ def mice_by_group(
     df_mice_group_col: str = "group",
     df_mice_mouse_col: str = "mouse_name",
 ) -> Dict[str, np.ndarray]:
+    """
+    Get a dictionary with groups as keys and mice in each group as values.
+
+    Args:
+        df_mice (pd.DataFrame): Dataframe of mice metadata.
+        df_mice_group_col (str, optional): Name of the column with group information. Defaults to "group".
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+
+    Returns:
+        Dict[str, np.ndarray]: Dictionary with groups as keys and mice in each group as values.
+    """
     mice_by_group = (
         df_mice.groupby(df_mice_group_col)[df_mice_mouse_col].unique().to_dict()
     )
@@ -23,6 +34,21 @@ def neurons_by_group(
     df_neurons_neuron_col: str = "cell_id",
     map_int_to_str: bool = True,
 ) -> Dict[str, np.ndarray]:
+    """
+    Get a dictionary with groups as keys and neurons in each group as values.
+
+    Args:
+        df_neurons (pd.DataFrame): Dataframe of neuron metadata.
+        df_mice (pd.DataFrame): Dataframe of mice metadata.
+        df_mice_group_col (str, optional): Name of the column with group information. Defaults to "group".
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
+        map_int_to_str (bool, optional): Whether to map neuron names to strings. Defaults to True.
+
+    Returns:
+        Dict[str, np.ndarray]: Dictionary with groups as keys and neurons in each group as values.
+    """
     mice_dict = mice_by_group(
         df_mice=df_mice,
         df_mice_group_col=df_mice_group_col,
@@ -48,6 +74,20 @@ def neurons_by_mouse(
     df_neurons_neuron_col: str = "cell_id",
     map_int_to_str: bool = True,
 ) -> Dict[str, np.ndarray]:
+    """
+    Get a dictionary with mouse names as keys and neurons in each mouse as values.
+
+    Args:
+        df_neurons (pd.DataFrame): Dataframe of neuron metadata.
+        df_mice (pd.DataFrame): Dataframe of mice metadata.
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
+        map_int_to_str (bool, optional): Whether to map neuron names to strings. Defaults to True.
+
+    Returns:
+        Dict[str, np.ndarray]: Dictionary with mouse names as keys and neurons in each mouse as values.
+    """
     by_mouse = {}
     for mouse in df_mice[df_mice_mouse_col].unique():
         mouse_neurons = df_neurons[df_neurons[df_neurons_mouse_col] == mouse][
@@ -67,9 +107,25 @@ def traces_by_group(
     df_mice_group_col: str = "group",
     df_mice_mouse_col: str = "mouse_name",
     df_neurons_mouse_col: str = "mouse_name",
-    df_traces_time_col: str = "time",
+    df_traces_time_col: Optional[str] = "time",
     map_int_to_str: bool = True,
 ) -> Dict[str, pd.DataFrame]:
+    """
+    Get a dictionary with groups as keys and traces in each group as values.
+    
+    Args:
+        df_traces (pd.DataFrame): Dataframe of traces.
+        df_neurons (pd.DataFrame): Dataframe of neuron metadata.
+        df_mice (pd.DataFrame): Dataframe of mice metadata.
+        df_mice_group_col (str, optional): Name of the column with group information. Defaults to "group".
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_traces_time_col (Optional[str], optional): Name of the column with time information in traces dataframe. Defaults to "time".
+        map_int_to_str (bool, optional): Whether to map neuron names to strings. Defaults to True.
+
+    Returns:
+        Dict[str, pd.DataFrame]: Dictionary with groups as keys and traces in each group as values.
+    """
     neurons_dict = neurons_by_group(
         df_neurons=df_neurons,
         df_mice=df_mice,
@@ -80,9 +136,12 @@ def traces_by_group(
     )
     traces_by_group = {}
     for group, neurons in neurons_dict.items():
-        traces_by_group[group] = df_traces[
-            [df_traces_time_col] + [c for c in df_traces.columns if c in neurons]
-        ]
+        if df_traces_time_col is None:
+            cols = [c for c in df_traces.columns if c in neurons]
+        else:
+            cols = [df_traces_time_col] + [c for c in df_traces.columns if c in neurons]
+
+        traces_by_group[group] = df_traces[cols]
     return traces_by_group
 
 
@@ -96,10 +155,26 @@ def traces_by_group_long(
     df_neurons_neuron_col: str = "cell_id",
     df_traces_long_neuron_col: str = "cell_id",
 ) -> pd.DataFrame:
+    """
+    Add group information to a long-format dataframe of traces.
+    
+    Args:
+        df_traces_long (pd.DataFrame): Long-format dataframe of traces.
+        df_neurons (pd.DataFrame): Dataframe of neuron metadata.
+        df_mice (pd.DataFrame): Dataframe of mice metadata.
+        df_mice_group_col (str, optional): Name of the column with group information. Defaults to "group".
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
+        df_traces_long_neuron_col (str, optional): Name of the column with neuron names in long-format dataframe. Defaults to "cell_id".
+
+    Returns:
+        pd.DataFrame: Long-format dataframe of traces with group information.
+    """
     df_neuron_groups = pd.merge(
         df_neurons,
         df_mice[[df_mice_mouse_col, df_mice_group_col]].drop_duplicates(),
-        right_pn=df_mice_mouse_col,
+        right_on=df_mice_mouse_col,
         left_on=df_neurons_mouse_col,
     )
     df_traces_long = pd.merge(
@@ -116,9 +191,22 @@ def traces_by_mouse(
     df_neurons: pd.DataFrame,
     df_neurons_mouse_col: str = "mouse_name",
     df_neurons_neuron_col: str = "cell_id",
-    df_traces_time_col: str = "time",
+    df_traces_time_col: Optional[str] = "time",
     map_int_to_str: bool = True,
 ) -> Dict[str, pd.DataFrame]:
+    """
+    Get a dictionary with mouse names as keys and traces in each mouse as values.
+    
+    Args:
+        df_traces (pd.DataFrame): Dataframe of traces.
+        df_neurons (pd.DataFrame): Dataframe of neuron metadata.
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
+        df_traces_time_col (Optional[str], optional): Name of the column with time information in traces dataframe
+
+    Returns:
+        Dict[str, pd.DataFrame]: Dictionary with mouse names as keys and traces in each mouse as values.
+    """
     traces_by_mouse = {}
 
     for mouse in df_neurons[df_neurons_mouse_col].unique():
@@ -127,13 +215,32 @@ def traces_by_mouse(
         ].unique()
         if map_int_to_str:
             mouse_neurons = [str(n) for n in mouse_neurons]
+
         cols = [c for c in df_traces.columns if c in mouse_neurons]
-        traces_by_mouse[mouse] = df_traces[[df_traces_time_col] + cols]
+        if df_traces_time_col is not None:
+            cols = [df_traces_time_col] + cols
+
+        traces_by_mouse[mouse] = df_traces[cols]
 
     return traces_by_mouse
 
 
 class GroupSplitter:
+    """
+    Class to split data by groups and mouse names.
+    
+    Args:
+        df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+        df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
+        df_mice_mouse_col (str, optional): Name of the column with mouse names. Defaults to "mouse_name".
+        df_mice_group_col (str, optional): Name of the column with group information. Defaults to "group".
+        df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
+        df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
+        df_traces_time_col (str, optional): Name of the column with time information in traces dataframe. Defaults to "time".
+        df_traces_long_neuron_col (str, optional): Name of the column with neuron names in long-format dataframe. Defaults to "cell_id".
+        excluded_groups (Optional[List[str]], optional): List of groups to exclude. Defaults to None.
+        map_int_to_str (bool, optional): Whether to map neuron names to strings. Defaults to True.
+    """
     def __init__(
         self,
         df_mice: Optional[pd.DataFrame] = None,
@@ -167,7 +274,13 @@ class GroupSplitter:
         ].copy()
 
     @property
-    def groups(self):
+    def groups(self) -> np.ndarray:
+        """
+        Get unique group names.
+
+        Returns:
+            np.ndarray: Unique group names.
+        """
         if self.df_mice is None:
             raise ValueError("df_mice must be provided")
         df_mice = self.df_mice
@@ -176,6 +289,12 @@ class GroupSplitter:
 
     @property
     def mice(self):
+        """
+        Get unique mouse names.
+        
+        Returns:
+            np.ndarray: Unique mouse names.
+        """
         if self.df_mice is None:
             raise ValueError("df_mice must be provided")
         df_mice = self.df_mice
@@ -185,6 +304,12 @@ class GroupSplitter:
     def mice_by_group(
         self, df_mice: Optional[pd.DataFrame] = None
     ) -> Dict[str, np.ndarray]:
+        """
+        Get a dictionary with groups as keys and mice in each group as values.
+        
+        Args:
+            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+        """
         if df_mice is None:
             assert self.df_mice is not None, "df_mice must be provided"
             df_mice = self.df_mice
@@ -202,6 +327,16 @@ class GroupSplitter:
         df_neurons: Optional[pd.DataFrame] = None,
         df_mice: Optional[pd.DataFrame] = None,
     ) -> Dict[str, np.ndarray]:
+        """
+        Get a dictionary with groups as keys and neurons in each group as values.
+        
+        Args:
+            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
+            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+            
+        Returns:
+            Dict[str, np.ndarray]: Dictionary with groups as keys and neurons in each group as values.
+        """
         if df_neurons is None:
             assert self.df_neurons is not None, "df_neurons must be provided"
             df_neurons = self.df_neurons
@@ -227,6 +362,16 @@ class GroupSplitter:
         df_neurons: Optional[pd.DataFrame] = None,
         df_mice: Optional[pd.DataFrame] = None,
     ) -> Dict[str, np.ndarray]:
+        """
+        Get a dictionary with mouse names as keys and neurons in each mouse as values.
+        
+        Args:
+            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
+            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+            
+        Returns:
+            Dict[str, np.ndarray]: Dictionary with mouse names as keys and neurons in each mouse as values.
+        """
         if df_neurons is None:
             assert self.df_neurons is not None, "df_neurons must be provided"
             df_neurons = self.df_neurons
@@ -252,6 +397,18 @@ class GroupSplitter:
         df_mice: Optional[pd.DataFrame] = None,
         df_traces_time_col: Optional[str] = None,
     ) -> Dict[str, pd.DataFrame]:
+        """
+        Get a dictionary with groups as keys and traces in each group as values.
+        
+        Args:
+            df_traces (pd.DataFrame): Dataframe of traces.
+            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
+            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+            df_traces_time_col (Optional[str], optional): Name of the column with time information in traces dataframe. Defaults to None.
+            
+        Returns:
+            Dict[str, pd.DataFrame]: Dictionary with groups as keys and traces in each group as values.
+        """
         if df_neurons is None:
             assert self.df_neurons is not None, "df_neurons must be provided"
             df_neurons = self.df_neurons
