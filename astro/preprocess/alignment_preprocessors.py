04f3f28c711abea7f2392d0f5c9515b72b9dbbd0
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2024-03-07 14:31:47 -0500

    Moved preprocess to a directory

---
+++ astro/preprocess/alignment_preprocessors.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from .config import PreprocessConfig
+from abc import ABC, abstractmethod
+
+
+class BasePreprocessor(ABC):
+    def __init__(self, preprocess_config: PreprocessConfig):
+        self.preprocess_config = preprocess_config
+
+    @abstractmethod
+    def preprocess_raw_traces(self, df_traces: pd.DataFrame) -> pd.DataFrame:
+        ...
+
+    @abstractmethod
+    def preprocess_events(self, df_events: pd.DataFrame) -> pd.DataFrame:
+        ...
+
+    @abstractmethod
+    def align_traces(
+        self, df_traces: pd.DataFrame, df_events: pd.DataFrame
+    ) -> pd.DataFrame:
+        ...
+
+    @abstractmethod
+    def preprocess_aligned_traces(self, df_traces: pd.DataFrame) -> pd.DataFrame:
+        ...
+
+
+class AlignmentPreprocessor(BasePreprocessor):
+    def preprocess_raw_traces(self, df_traces: pd.DataFrame) -> pd.DataFrame:
+        if self.preprocess_config.trace_preprocessor_load is not None:
+            df_traces = self.preprocess_config.trace_preprocessor_load(df_traces)
+        if self.preprocess_config.trace_rotator is not None:
+            df_traces = self.preprocess_config.trace_rotator(df_traces)
+        return df_traces
+
+    def preprocess_events(self, df_events: pd.DataFrame) -> pd.DataFrame:
+        if self.preprocess_config.event_preprocessor is not None:
+            df_events = self.preprocess_config.event_preprocessor(df_events)
+        return df_events
+
+    def align_traces(
+        self, df_traces: pd.DataFrame, df_events: pd.DataFrame
+    ) -> pd.DataFrame:
+        return self.preprocess_config.aligner.align(df_traces, df_events)
+
+    def preprocess_aligned_traces(self, df_traces: pd.DataFrame) -> pd.DataFrame:
+        if self.preprocess_config.trace_preprocessor_post_alignment is not None:
+            df_traces = self.preprocess_config.trace_preprocessor_post_alignment(
+                df_traces
+            )
+        return df_traces
+
+    def __call__(
+        self, df_traces: pd.DataFrame, df_events: pd.DataFrame
+    ) -> pd.DataFrame:
+        df_traces = self.preprocess_raw_traces(df_traces)
+        df_events = self.preprocess_events(df_events)
+        df_traces = self.align_traces(df_traces, df_events)
+        df_traces = self.preprocess_aligned_traces(df_traces)
+        return df_traces
---
+++ astro/preprocess/config.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from trace_minder.align import GroupedAligner
+from trace_minder.preprocess import TracePreprocessor
+from trace_minder.surrogates import Rotater, TraceSampler
+from .raw_preprocessors import GroupedEventPreprocessor
+
+
+@dataclass
+class PreprocessConfig:
+    """
+    Container for preprocessing configuration.
+
+    Args:
+        aligner (GroupedAligner): trace_minder.align.GroupedAligner instance
+        trace_preprocessor_load (TracePreprocessor, optional): trace_minder.preprocess.TracePreprocessor instance. Defaults to None.
+        event_preprocessor (GroupedEventPreprocessor, optional): trace_minder.preprocess.GroupedEventPreprocessor instance. Defaults to None.
+        trace_rotator (Rotater, optional): trace_minder.surrogates.Rotater instance. Defaults to None.
+        trace_sampler (TraceSampler, optional): trace_minder.surrogates.TraceSampler instance. Defaults to None.
+        trace_preprocessor_post_alignment (TracePreprocessor, optional): trace_minder.preprocess.TracePreprocessor instance. Defaults to None.
+    """
+
+    aligner: GroupedAligner
+    trace_preprocessor_load: TracePreprocessor | None = None
+    event_preprocessor: GroupedEventPreprocessor | None = None
+    trace_rotator: Rotater | None = None
+    trace_sampler: TraceSampler | None = None
+    trace_preprocessor_post_alignment: TracePreprocessor | None = None
