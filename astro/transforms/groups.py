0602165bf1639f2e4b37837289b640d052c6de9e
Ruairi-osul <ruairi.osullivan@univ.ox.ac.uk>
2024-03-07 13:28:03 -0500

    Updated group splitter to work with retained mappings closses #1 #2

--- astro/transforms/groups.py
+++ astro/transforms/groups.py
@@ -303,7 +303,6 @@ class GroupSplitter:
         df_neurons_mouse_col (str, optional): Name of the column with mouse names in neuron dataframe. Defaults to "mouse_name".
         df_neurons_neuron_col (str, optional): Name of the column with neuron names. Defaults to "cell_id".
         df_traces_time_col (str, optional): Name of the column with time information in traces dataframe. Defaults to "time".
-        df_traces_long_neuron_col (str, optional): Name of the column with neuron names in long-format dataframe. Defaults to "cell_id".
         excluded_groups (Optional[List[str]], optional): List of groups to exclude. Defaults to None.
         map_int_to_str (bool, optional): Whether to map neuron names to strings. Defaults to True.
     """
@@ -317,33 +316,72 @@ class GroupSplitter:
         df_neurons_mouse_col: str = "mouse_name",
         df_neurons_neuron_col: str = "cell_id",
         df_traces_time_col: str = "time",
-        df_traces_long_neuron_col: str = "cell_id",
         excluded_groups: Optional[List[str]] = None,
         map_int_to_str: bool = True,
         permute_neurons: bool = False,
         permute_mice: bool = False,
         equalize_neuron_numbers: bool = False,
     ):
-        self.df_mice = df_mice
-        self.df_neurons = df_neurons
+        self._df_mice = df_mice
+        self._df_neurons = df_neurons
+
         self.df_mice_mouse_col = df_mice_mouse_col
         self.df_mice_group_col = df_mice_group_col
         self.df_neurons_mouse_col = df_neurons_mouse_col
         self.df_neurons_neuron_col = df_neurons_neuron_col
         self.df_traces_time_col = df_traces_time_col
-        self.df_traces_long_neuron_col = df_traces_long_neuron_col
+        
         self.map_int_to_str = map_int_to_str
         self.permute_neurons = permute_neurons
         self.permute_mice = permute_mice
         self.equalize_neuron_numbers = equalize_neuron_numbers
 
+        self._mouse_group_mapping: dict[str, np.ndarray] | None = None
+        self._neuron_group_mapping: dict[str, np.ndarray] | None = None
+        self._neuron_mouse_mapping: dict[str, np.ndarray] | None = None
+
         if excluded_groups is None:
             excluded_groups = []
         self.excluded_groups = excluded_groups
 
-    def _exclude_groups(self, df_mice: pd.DataFrame) -> pd.DataFrame:
-        return df_mice[
-            ~df_mice[self.df_mice_group_col].isin(self.excluded_groups)
+    def set_df_mice(
+        self,
+        df_mice: pd.DataFrame,
+        mouse_col: str | None = None,
+        group_col: str | None = None,
+    ) -> None:
+        self._df_mice = df_mice
+        if mouse_col is not None:
+            self.df_mice_mouse_col = mouse_col
+        if group_col is not None:
+            self.df_mice_group_col = group_col
+
+    def set_df_neurons(
+        self,
+        df_neurons: pd.DataFrame,
+        mouse_col: str | None = None,
+        neuron_col: str | None = None,
+    ) -> None:
+        self._df_neurons = df_neurons
+        if mouse_col is not None:
+            self.df_neurons_mouse_col = mouse_col
+        if neuron_col is not None:
+            self.df_neurons_neuron_col = neuron_col
+
+    @property
+    def df_mice(self) -> pd.DataFrame:
+        if self._df_mice is None:
+            raise ValueError("df_mice not set")
+        return self._df_mice[
+            ~self._df_mice[self.df_mice_group_col].isin(self.excluded_groups)
+        ].copy()
+
+    @property
+    def df_neurons(self) -> pd.DataFrame:
+        if self._df_neurons is None:
+            raise ValueError("df_neurons not set")
+        return self._df_neurons.loc[
+            lambda x: x[self.df_neurons_mouse_col].isin(self.mice)
         ].copy()
 
     @property
@@ -354,192 +392,167 @@ class GroupSplitter:
         Returns:
             np.ndarray: Unique group names.
         """
-        if self.df_mice is None:
-            raise ValueError("df_mice must be provided")
-        df_mice = self.df_mice
-        df_mice = self._exclude_groups(df_mice)
-        return df_mice[self.df_mice_group_col].unique()
+        return self.df_mice[self.df_mice_group_col].unique()
 
     @property
-    def mice(self):
+    def mice(self) -> np.ndarray:
         """
         Get unique mouse names.
 
         Returns:
             np.ndarray: Unique mouse names.
         """
-        if self.df_mice is None:
-            raise ValueError("df_mice must be provided")
-        df_mice = self.df_mice
-        df_mice = self._exclude_groups(df_mice)
-        return df_mice[self.df_mice_mouse_col].unique()
+        return self.df_mice[self.df_mice_mouse_col].unique()
 
+    @property
+    def neurons(self) -> np.ndarray:
+        return self.df_neurons[self.df_neurons_neuron_col].unique()
+
+    @property
     def mice_by_group(
-        self, df_mice: Optional[pd.DataFrame] = None
-    ) -> Dict[str, np.ndarray]:
+        self,
+    ) -> dict[str, np.ndarray]:
         """
         Get a dictionary with groups as keys and mice in each group as values.
 
-        Args:
-            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
+        Returns:
+            dict[str, np.ndarray]: Dictionary with groups as keys and mice in each group as values.
         """
-        if df_mice is None:
-            assert self.df_mice is not None, "df_mice must be provided"
-            df_mice = self.df_mice
+        if self._mouse_group_mapping is None:
+            self._mouse_group_mapping = mice_by_group(
+                df_mice=self.df_mice,
+                df_mice_group_col=self.df_mice_group_col,
+                df_mice_mouse_col=self.df_mice_mouse_col,
+                permute_mice=self.permute_mice,
+            )
+        return self._mouse_group_mapping
 
-        df_mice = self._exclude_groups(df_mice)
-
-        return mice_by_group(
-            df_mice=df_mice,
-            df_mice_group_col=self.df_mice_group_col,
-            df_mice_mouse_col=self.df_mice_mouse_col,
-            permute_mice=self.permute_mice,
-        )
-
-    def neurons_by_group(
-        self,
-        df_neurons: Optional[pd.DataFrame] = None,
-        df_mice: Optional[pd.DataFrame] = None,
-    ) -> Dict[str, np.ndarray]:
+    @property
+    def neurons_by_group(self) -> dict[str, np.ndarray]:
         """
         Get a dictionary with groups as keys and neurons in each group as values.
 
-        Args:
-            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
-            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
-
         Returns:
-            Dict[str, np.ndarray]: Dictionary with groups as keys and neurons in each group as values.
+            dict[str, np.ndarray]: Dictionary with groups as keys and neurons in each group as values.
         """
-        if df_neurons is None:
-            assert self.df_neurons is not None, "df_neurons must be provided"
-            df_neurons = self.df_neurons
+        if self._neuron_group_mapping is None:
+            self._neuron_group_mapping = neurons_by_group(
+                df_neurons=self.df_neurons,
+                df_mice=self.df_mice,
+                df_mice_group_col=self.df_mice_group_col,
+                df_mice_mouse_col=self.df_mice_mouse_col,
+                df_neurons_mouse_col=self.df_neurons_mouse_col,
+                df_neurons_neuron_col=self.df_neurons_neuron_col,
+                map_int_to_str=self.map_int_to_str,
+                permute_neurons=self.permute_neurons,
+                equalize_neuron_numbers=self.equalize_neuron_numbers,
+            )
 
-        if df_mice is None:
-            assert self.df_mice is not None, "df_mice must be provided"
-            df_mice = self.df_mice
+        return self._neuron_group_mapping
 
-        df_mice = self._exclude_groups(df_mice)
-
-        return neurons_by_group(
-            df_neurons=df_neurons,
-            df_mice=df_mice,
-            df_mice_group_col=self.df_mice_group_col,
-            df_mice_mouse_col=self.df_mice_mouse_col,
-            df_neurons_mouse_col=self.df_neurons_mouse_col,
-            df_neurons_neuron_col=self.df_neurons_neuron_col,
-            map_int_to_str=self.map_int_to_str,
-            permute_neurons=self.permute_neurons,
-            equalize_neuron_numbers=self.equalize_neuron_numbers,
-        )
-
+    @property
     def neurons_by_mouse(
         self,
-        df_neurons: Optional[pd.DataFrame] = None,
-        df_mice: Optional[pd.DataFrame] = None,
-    ) -> Dict[str, np.ndarray]:
+    ) -> dict[str, np.ndarray]:
         """
         Get a dictionary with mouse names as keys and neurons in each mouse as values.
 
-        Args:
-            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
-            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
-
         Returns:
-            Dict[str, np.ndarray]: Dictionary with mouse names as keys and neurons in each mouse as values.
+            dict[str, np.ndarray]: Dictionary with mouse names as keys and neurons in each mouse as values.
         """
-        if df_neurons is None:
-            assert self.df_neurons is not None, "df_neurons must be provided"
-            df_neurons = self.df_neurons
-
-        if df_mice is None:
-            assert self.df_mice is not None, "df_mice must be provided"
-            df_mice = self.df_mice
-        df_mice = self._exclude_groups(df_mice)
-
-        return neurons_by_mouse(
-            df_neurons=df_neurons,
-            df_mice=df_mice,
-            df_mice_mouse_col=self.df_mice_mouse_col,
-            df_neurons_mouse_col=self.df_neurons_mouse_col,
-            df_neurons_neuron_col=self.df_neurons_neuron_col,
-            map_int_to_str=self.map_int_to_str,
-            equalize_neuron_numbers=self.equalize_neuron_numbers,
-        )
+        if self._neuron_mouse_mapping is None:
+            self._neuron_mouse_mapping = neurons_by_mouse(
+                df_neurons=self.df_neurons,
+                df_mice=self.df_mice,
+                df_mice_mouse_col=self.df_mice_mouse_col,
+                df_neurons_mouse_col=self.df_neurons_mouse_col,
+                df_neurons_neuron_col=self.df_neurons_neuron_col,
+                map_int_to_str=self.map_int_to_str,
+                equalize_neuron_numbers=self.equalize_neuron_numbers,
+            )
+        return self._neuron_mouse_mapping
+    
+    def refresh_mappings(self):
+        self._mouse_group_mapping = None
+        self._neuron_group_mapping = None
+        self._neuron_mouse_mapping = None
 
     def traces_by_group(
         self,
         df_traces: pd.DataFrame,
-        df_neurons: Optional[pd.DataFrame] = None,
-        df_mice: Optional[pd.DataFrame] = None,
         df_traces_time_col: Optional[str] = None,
-    ) -> Dict[str, pd.DataFrame]:
+    ) -> dict[str, pd.DataFrame]:
         """
         Get a dictionary with groups as keys and traces in each group as values.
 
         Args:
             df_traces (pd.DataFrame): Dataframe of traces.
-            df_neurons (Optional[pd.DataFrame], optional): Dataframe of neuron metadata. Defaults to None.
-            df_mice (Optional[pd.DataFrame], optional): Dataframe of mice metadata. Defaults to None.
             df_traces_time_col (Optional[str], optional): Name of the column with time information in traces dataframe. Defaults to None.
 
         Returns:
             Dict[str, pd.DataFrame]: Dictionary with groups as keys and traces in each group as values.
         """
-        if df_neurons is None:
-            assert self.df_neurons is not None, "df_neurons must be provided"
-            df_neurons = self.df_neurons
-
-        if df_mice is None:
-            assert self.df_mice is not None, "df_mice must be provided"
-            df_mice = self.df_mice
 
         if df_traces_time_col is None:
             df_traces_time_col = self.df_traces_time_col
-        df_mice = self._exclude_groups(df_mice)
 
-        return traces_by_group(
-            df_traces=df_traces,
-            df_neurons=df_neurons,
-            df_mice=df_mice,
-            df_mice_group_col=self.df_mice_group_col,
-            df_mice_mouse_col=self.df_mice_mouse_col,
-            df_neurons_mouse_col=self.df_neurons_mouse_col,
-            df_traces_time_col=df_traces_time_col,
-            map_int_to_str=self.map_int_to_str,
-            permute_neurons=self.permute_neurons,
-            equalize_neuron_numbers=self.equalize_neuron_numbers,
-        )
+        trace_dict = {}
+        for group, neurons in self.neurons_by_group.items():
+            cols = [c for c in df_traces.columns if c in neurons]
+            if df_traces_time_col is not None:
+                cols = [df_traces_time_col] + cols
+            trace_dict[group] = df_traces[cols]
+        return trace_dict
 
+
+    def traces_by_mouse(
+        self,
+        df_traces: pd.DataFrame,
+        df_traces_time_col: Optional[str] = None,
+    ) -> dict[str, pd.DataFrame]:
+        """
+        Get a dictionary with mouse names as keys and traces in each mouse as values.
+
+        Args:
+            df_traces (pd.DataFrame): Dataframe of traces.
+            df_traces_time_col (Optional[str], optional): Name of the column with time information in traces dataframe. Defaults to None.
+
+        Returns:
+            Dict[str, pd.DataFrame]: Dictionary with mouse names as keys and traces in each mouse as values.
+        """
+        if df_traces_time_col is None:
+            df_traces_time_col = self.df_traces_time_col
+
+        trace_dict = {}
+        for mouse, neurons in self.neurons_by_mouse.items():
+            cols = [c for c in df_traces.columns if c in neurons]
+            if df_traces_time_col is not None:
+                cols = [df_traces_time_col] + cols
+            trace_dict[mouse] = df_traces[cols]
+        return trace_dict
+    
     def traces_by_group_long(
         self,
         df_traces_long: pd.DataFrame,
-        df_neurons: Optional[pd.DataFrame] = None,
-        df_mice: Optional[pd.DataFrame] = None,
-        df_traces_long_time_col: Optional[str] = None,
+        df_traces_long_neuron_col: str | None = None,
     ) -> pd.DataFrame:
-        if df_neurons is None:
-            assert self.df_neurons is not None, "df_neurons must be provided"
-            df_neurons = self.df_neurons
+        """
+        Add group information to a long-format dataframe of traces.
 
-        if df_mice is None:
-            assert self.df_mice is not None, "df_mice must be provided"
-            df_mice = self.df_mice
+        Args:
+            df_traces_long (pd.DataFrame): Long-format dataframe of traces.
+            df_traces_long_time_col (str, optional): Name of the column with time information in long-format traces dataframe
 
-        if df_traces_long_time_col is None:
-            df_traces_long_time_col = self.df_traces_time_col
+        Returns:
+            pd.DataFrame: Long-format dataframe of traces with group information.
+        """
 
-        df_mice = self._exclude_groups(df_mice)
+        if df_traces_long_neuron_col is None:
+            df_traces_long_neuron_col = self.df_neurons_neuron_col
 
-        return traces_by_group_long(
-            df_traces_long=df_traces_long,
-            df_neurons=df_neurons,
-            df_mice=df_mice,
-            df_mice_group_col=self.df_mice_group_col,
-            df_mice_mouse_col=self.df_mice_mouse_col,
-            df_neurons_mouse_col=self.df_neurons_mouse_col,
-            df_neurons_neuron_col=self.df_neurons_neuron_col,
-            df_traces_long_neuron_col=self.df_traces_long_neuron_col,
-            permute_neurons=self.permute_neurons,
-            equalize_neuron_numbers=self.equalize_neuron_numbers,
-        )
+        neuron_group_mapper = self.neurons_by_group
+        neurons_by_group_inv = {neuron: group for group, neurons in neuron_group_mapper.items() for neuron in neurons}
+
+        df_traces_long[self.df_mice_group_col] = df_traces_long[df_traces_long_neuron_col].map(neurons_by_group_inv)
+
+        return df_traces_long
